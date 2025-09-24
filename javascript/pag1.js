1618daafe9b99bfd2c713579e24e923055585116
Dgzinn0110 <juandersonfreitasvieira@gmail.com>
2025-09-24 17:29:17 -0300

    Refactor code structure for improved readability and maintainability

--- javascript/pag1.js
+++ javascript/pag1.js
@@ -1,15 +1,18 @@
-//constantes dos botões da top-bar
+//constantes dos botões
 const btncultura = document.getElementById('cultura')
 const btnsociedade = document.getElementById('sociedade')
 const btncapital = document.getElementById('capital')
 const btneconomia = document.getElementById('economia')
+const seta = document.getElementById('seta')
+const icone = document.getElementById('icone')
+let setaa = false
 
 //variáveis de para onde será escrolado dependendo do botão
 let culturposi = 0;
 let sociedadeposi = 100;
 let capitalposi = 200;
 let economiaposi = 300;
-
+seta.style.top = `5000px`
 //eventlistener botões top-bar
 btncultura.addEventListener('click',()=>{
     window.scrollTo({ top: culturposi, behavior: 'smooth' })
@@ -22,4 +25,25 @@ btncapital.addEventListener('click',()=>{
 })
 btneconomia.addEventListener('click',()=>{
     window.scrollTo({ top: economiaposi, behavior: 'smooth' })
+})
+seta.addEventListener('click',()=>{
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    seta.style.top = `200vh`;
+    setaa = true
+
+})
+document.addEventListener('scroll',()=>{
+    if(window.scrollY > 400){
+        console.log(window.scrollY)
+        if(setaa == false) seta.style.top = `86vh`; 
+        setaa = true
+    }
+    else{
+        seta.style.top = `200vh`;
+    setaa = false
+}
+})
+
+icone.addEventListener('click',()=>{
+    window.location.href = '/html/pag1.html' 
 })
