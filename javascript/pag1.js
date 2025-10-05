95e2aa6be2bb236d9500d3e290b2c440e361bb62
RovaronDev <vamp35308@gmail.com>
2025-10-05 20:17:46 -0300

    Revert "Co-authored-by: LuanTestaDEV <LuanTestaDEV@users.noreply.github.com>"
    This reverts commit 27aea3ee6719288ad08b7f9601b6f048269f2c7c.

--- javascript/pag1.js
+++ javascript/pag1.js
@@ -1,12 +1,11 @@
 //constantes dos botões
 const btncultura = document.getElementById('cultura')
 const btnsociedade = document.getElementById('sociedade')
-
+const btncapital = document.getElementById('capital')
 const btneconomia = document.getElementById('economia')
 const seta = document.getElementById('seta')
 const icone = document.getElementById('icone')
 let setaa = false
-const topbar = document.getElementById('topbar')
 
 //variáveis de para onde será escrolado dependendo do botão
 let culturposi = 0;
@@ -14,12 +13,6 @@ let sociedadeposi = 100;
 let capitalposi = 200;
 let economiaposi = 300;
 seta.style.top = `5000px`
-
-document.addEventListener('DOMContentLoaded', (event) => { 
-        window.scrollTo(0,0)
-        topbar.classList.add('topmaior')
-})
-
 //eventlistener botões top-bar
 btncultura.addEventListener('click',()=>{
     window.scrollTo({ top: culturposi, behavior: 'smooth' })
@@ -27,7 +20,9 @@ btncultura.addEventListener('click',()=>{
 btnsociedade.addEventListener('click',()=>{
     window.scrollTo({ top: sociedadeposi, behavior: 'smooth' })
 })
-
+btncapital.addEventListener('click',()=>{
+    window.scrollTo({ top: capitalposi, behavior: 'smooth' })
+})
 btneconomia.addEventListener('click',()=>{
     window.scrollTo({ top: economiaposi, behavior: 'smooth' })
 })
@@ -51,15 +46,4 @@ document.addEventListener('scroll',()=>{
 
 icone.addEventListener('click',()=>{
     window.location.href = '/html/pag1.html' 
-})
-document.addEventListener('scroll',()=>{
-     if(window.scrollY < 40){
-        if(topbar.classList.add('topmaior') = false){
-            topbar.classList.add('topmaior')
-        }
-        
-    }else{
-        topbar.classList.remove('topmaior')
-    }
-
 })
